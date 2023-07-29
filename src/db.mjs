f751be8d6e576b209753d7ae8599febfeac3a08b
sc8341 <106842392+sc8341@users.noreply.github.com>
2023-07-28 20:19:28 -0400

    Add files via upload

---
+++ src/db.mjs
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import mongooseSlugPlugin from 'mongoose-slug-plugin';
+
+// TODO: add schemas
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, unique: true, required: true}
+});
+
+const ArticleSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: {type: String, required: true},
+    url: {type: String, required: true},
+    description: {type: String, required: true}
+});
+
+// TODO: configure plugin
+ArticleSchema.plugin(mongooseSlugPlugin, {tmpl: '<%=title%>'});
+
+// TODO: register models
+mongoose.model('User', UserSchema);
+mongoose.model('Article', ArticleSchema);
+
+mongoose.connect('mongodb://localhost/hw05');
